33cf02029d1e9a4a6b2f9caa3eb2dc03f099281d
M. G. SUDHARSAN <154258773+sudharsanmg2005@users.noreply.github.com>
2025-07-04 09:14:10 +0530

    Add files via upload

---
+++ DAY 2/day1.js
@@ -0,0 +1,20 @@
+// console.log("Hello, SUGANTH ST!");
+// console.log("1, 2, 3, 4, 5, ");
+// let a = 10;
+// var a = 10;
+// console.log(a);
+//  let product key = "abc_efgh-ijkl-1234"
+//  productKey = 'Earth is flar XD'
+  
+//  const a;
+//  a= 20;
+//     console.log("initializating product key:", productKey);
+
+
+// console.log("Product key:", productKey);
+// console.log("Value of a:", a);
+
+// get ElementBy Id("header").innerHTML = "SUGANTH ST";
+// document.getElementById("header").innerHTML = "SUGANTH ST";
+// document.getElementById("header").style.color = "blue";
+
---
+++ DAY 2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("para1").innerHTML = "Hi";
+
+let name = "SUDHARSAN!!";
+document.getElementById("para2").innerHTML = `Hello from ${name} `;
